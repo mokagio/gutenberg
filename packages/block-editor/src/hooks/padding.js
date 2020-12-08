dc32817e6b084f6fa61ac09923172bdb365eec9b
Ben Dwyer <ben@scruffian.com>
2020-12-08 18:48:23 +0000

    Reset block spacing back to the default value rather than removing it

--- packages/block-editor/src/hooks/padding.js
+++ packages/block-editor/src/hooks/padding.js
@@ -3,7 +3,7 @@
  */
 import { __ } from '@wordpress/i18n';
 import { Platform } from '@wordpress/element';
-import { getBlockSupport } from '@wordpress/blocks';
+import { getBlockSupport, getBlockType } from '@wordpress/blocks';
 import { __experimentalBoxControl as BoxControl } from '@wordpress/components';
 
 /**
@@ -65,6 +65,15 @@ export function PaddingEdit( props ) {
 		} );
 	};
 
+	const onReset = () => {
+		const blockType = getBlockType( blockName );
+		const defaultSettings = blockType?.attributes?.style?.default;
+
+		setAttributes( {
+			style: cleanEmptyObject( defaultSettings ),
+		} );
+	};
+
 	return Platform.select( {
 		web: (
 			<>
@@ -72,6 +81,7 @@ export function PaddingEdit( props ) {
 					values={ style?.spacing?.padding }
 					onChange={ onChange }
 					onChangeShowVisualizer={ onChangeShowVisualizer }
+					onReset={ onReset }
 					label={ __( 'Padding' ) }
 					units={ units }
 				/>
